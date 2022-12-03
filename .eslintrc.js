807f6cb870c2a51d7fd6de96eefb8c6a2bc2e210
Manelitu <emanuelwork1@gmail.com>
2022-12-03 11:45:25 -0300

    chore: add prettier in eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,8 +6,25 @@ module.exports = {
     'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:import/typescript',
+    'prettier',
+    'plugin:prettier/recommended',
   ],
   parser: '@typescript-eslint/parser',
+  overrides: [{
+    files: ['*.ts', '*.tsx'], // Your TypeScript files extension
+
+      // As mentioned in the comments, you should extend TypeScript plugins here,
+      // instead of extending them outside the `overrides`.
+      // If you don't want to extend any rules, you don't need an `extends` attribute.
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+      ],
+
+      parserOptions: {
+        project: ['./tsconfig.json'], // Specify it only for TypeScript files
+      },
+  }],
   plugins: ['@typescript-eslint', 'prettier', 'eslint-plugin-import-helpers'],
   settings: {
     'import/parsers': {
