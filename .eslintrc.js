e1b60b7d527f1353463b29afeb9a7dad04181219
Manelitu <emanuelwork1@gmail.com>
2022-12-03 12:33:36 -0300

    chore: eslint no-console rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -38,6 +38,7 @@ module.exports = {
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
     'import/no-extraneous-dependencies': [2, { devDependencies: ['**/test.tsx', '**/test.ts'] }],
     '@typescript-eslint/indent': [2, 2],
+    'no-console': 'off',
     'import/extensions': [
       'error',
       'ignorePackages',
