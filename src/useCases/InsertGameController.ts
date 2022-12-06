e1b270b922c0fab0284946b95104dd4110387086
Manelitu <emanuelwork1@gmail.com>
2022-12-06 19:19:45 -0300

    feat: create InsertGame controller

---
+++ src/useCases/InsertGameController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+
+import { IGameDTO } from '../entities/Game';
+import { InsertGameUseCase } from './InsertGameUseCase';
+
+class InsertGameController {
+  constructor(private useCase: InsertGameUseCase) {}
+
+  async handle(req: Request, res: Response): Promise<Response> {
+    const game: IGameDTO = req.body as IGameDTO;
+    await this.useCase.execute(game);
+    return res.status(201).json({ message: 'Game registered' });
+  }
+}
+
+export { InsertGameController };
