7b60cd01bbdfc0bf9f1caa68ba165c94dd21368a
Manelitu <emanuelwork1@gmail.com>
2022-12-07 18:47:20 -0300

    refactor: exclude ambiguous type on InsertGameController

--- src/useCases/InsertGameController.ts
+++ src/useCases/InsertGameController.ts
@@ -7,7 +7,7 @@ export class InsertGameController {
   constructor(private useCase: InsertGameUseCase) {}
 
   async handle(req: Request, res: Response): Promise<Response> {
-    const game: IGameDTO = req.body as IGameDTO;
+    const game = req.body as IGameDTO;
     await this.useCase.execute(game);
     return res.status(201).json({ message: 'Game registered' });
   }
