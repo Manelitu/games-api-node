4aaf7e45b93c36ba035c52c79fe037d7d98b84b3
Manelitu <emanuelwork1@gmail.com>
2022-12-08 17:50:06 -0300

    refactor: removing get '/'

--- src/server.ts
+++ src/server.ts
@@ -7,10 +7,6 @@ const port = 5000;
 
 app.use(routes);
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
