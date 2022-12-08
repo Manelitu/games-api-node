3b4ff7a341658da9ed5fbbd2710991c78d25eda2
Manelitu <emanuelwork1@gmail.com>
2022-12-08 19:50:15 -0300

    fix: corretion prisma doesnt find .env file

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+import 'dotenv/config';
 
 import { routes } from './routes';
 
 const app = express();
 const port = 5000;
 
+app.use(express.json());
 app.use(routes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(`App listening at port ${port}`);
 });
